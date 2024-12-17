9175edd98c2d253d9d8ab31d39cacee2ec9cc8d7
walid lhaila <walidlhaila00@gmail.com>
2024-12-17 17:23:51 +0100

    Feat: ADD DELETE HR FROM DATABASE

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,29 @@
-  import { Body, Controller, Inject, Post, Req } from '@nestjs/common';
-  import { ClientProxy } from '@nestjs/microservices';
+import {Body, Controller, Delete, HttpException, HttpStatus, Inject, Param, Post, Req} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
-  @Controller('SIRH')
-  export class AppController {
-    constructor( @Inject('KeyCloak-Service') private readonly keycloakService: ClientProxy ) {}
+@Controller('SIRH')
+export class AppController {
+  constructor( @Inject('KeyCloak-Service') private readonly keycloakService: ClientProxy ) {}
 
-    @Post('login')
-    async login(@Body() body: { username: string; password: string }) {
-      return this.keycloakService.send({ cmd: 'login' }, body).toPromise();
-    }
-
-    @Post('createHR')
-    async createHR(@Req() req: Request) {
-      const token = req.headers['authorization'];
-      const payload = req.body;
-      console.log(token, payload);
-      return this.keycloakService.send({ cmd: 'createHR' }, { token, payload }).toPromise();
-    }
+  @Post('login')
+  async login(@Body() body: { username: string; password: string }) {
+    return this.keycloakService.send({ cmd: 'login' }, body).toPromise();
   }
+
+  @Post('createHR')
+  async createHR(@Req() req: Request) {
+    const token = req.headers['authorization'];
+    const payload = req.body;
+    return this.keycloakService.send({ cmd: 'createHR' }, { token, payload }).toPromise();
+  }
+
+  @Delete('/delete/:email')
+  async deleteHR(@Param('email') email: string, @Req() req: Request ): Promise<any> {
+    const token = req.headers['authorization'];
+    if(!token) {
+      throw new HttpException('No token provided', HttpStatus.UNAUTHORIZED);
+    }
+    return this.keycloakService.send({ cmd: 'deleteHR' }, { token, email }).toPromise()
+  }
+
+}
